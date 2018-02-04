72c39a708fbf0268f9e8b9e1a3dce33bc3c5c501
hanwen <1106002609@qq.com>
2018-02-04 16:14:11 +0800

    remove element

--- src/algorithm/RemoveElement.java
+++ src/algorithm/RemoveElement.java
@@ -2,6 +2,13 @@ package algorithm;
 
 public class RemoveElement {
 
+    /**
+     * 本题跟 RemoveDuplicatesfromSortedArray 题很相似，只是换了一种方法，将数组中的重复数字去重，跟给定的数字相同的数字从数组中移除
+     * 是相同的道理。数组去重是使用双指针移动法，本题也可以根据这种方法来处理。
+     * @param nums  数组
+     * @param val   要去除的数字
+     * @return  去重之后的数组长度
+     */
     public int removeElement(int[] nums, int val) {
 
         if(nums == null || nums.length == 0) {
@@ -9,6 +16,7 @@ public class RemoveElement {
         }
         int len = 0;
         for(int i = 0; i < nums.length; i++) {
+            // 数组中的数字跟给定的数字不同就往前移动
             if(nums[i] != val) {
                 nums[len++] = nums[i];
             }
