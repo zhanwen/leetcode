1b8b391a93d311082dd93f89da6631f254f888cd
hanwen <1106002609@qq.com>
2018-02-18 20:21:36 +0800

    update sqrt x

--- src/algorithm/Sqrt_x.java
+++ src/algorithm/Sqrt_x.java
@@ -11,9 +11,34 @@ public class Sqrt_x {
         return (int) Math.sqrt(x);
     }
 
+    /**
+     * 采用暴力破解的方法
+     * 从1开始到x结束。不断的进行计算比较
+     * @param x 输入的数
+     * @return  返回这个数的根
+     */
+    public int mySqrt2(int x) {
+        if(x == 0) return 0;
+        int left = 1;
+        int right = Integer.MAX_VALUE;
+        int ans = 0;
+        while(left <= right) {
+            int mid = left + (right - left)/2;
+
+            if(mid > x/mid) {
+                right = mid - 1;
+            }else {
+                ans = mid;
+                left = mid + 1;
+            }
+        }
+        return ans;
+    }
+
+
     public static void main(String[] args) {
         Sqrt_x s = new Sqrt_x();
-        int result = s.mySqrt(4);
+        int result = s.mySqrt2(9);
         System.out.println(result);
     }
 
