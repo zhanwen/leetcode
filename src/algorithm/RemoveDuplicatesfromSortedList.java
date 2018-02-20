fd321030884c807b505ece48bd28ae02ec990602
hanwen <1106002609@qq.com>
2018-02-20 22:03:36 +0800

    update remove duplicates from sorted list

--- src/algorithm/RemoveDuplicatesfromSortedList.java
+++ src/algorithm/RemoveDuplicatesfromSortedList.java
@@ -5,14 +5,22 @@ import java.util.Set;
 
 public class RemoveDuplicatesfromSortedList {
 
+    /**
+     * 从有序的列表中移除重复的数字
+     * @param head  原始链表
+     * @return  去重之后的链表
+     */
     public ListNode deleteDuplicates(ListNode head) {
 
        if(head == null) {
            return head;
        }
+       //前指针
        ListNode pre = head;
+       //当前指针
        ListNode current = head.next;
        while(current != null){
+           //如果前面指针的数值和当前指针的数值相同则将当前指针下一个元素前移，否则两个指针同时后移一步
            if(pre.val == current.val) {
                pre.next = current.next;
            }else {
