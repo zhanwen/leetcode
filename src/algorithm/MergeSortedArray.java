004ec89c14284f5d55f98e8337b5856368109468
hanwen <1106002609@qq.com>
2018-02-23 21:55:43 +0800

    merge sorted array

---
+++ src/algorithm/MergeSortedArray.java
@@ -0,0 +1,51 @@
+package algorithm;
+
+public class MergeSortedArray {
+
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+
+        if(n == 0) {
+            return;
+        }
+        if(m == 0 && n != 0) {
+            for(int i = 0; i < n; i++) {
+                nums1[i] = nums2[i];
+            }
+        }
+
+        int i = m-1;
+        int j = n-1;
+        int k = m+n-1;
+
+        while(i >= 0 && j >= 0) {
+            if(nums1[i] > nums2[j]) {
+                nums1[k] = nums1[i];
+                i--;
+            }else {
+                nums1[k] = nums2[j];
+                j--;
+            }
+            k--;
+        }
+
+        while(i >= 0) {
+            nums1[k] = nums1[i];
+            i--;
+            k--;
+        }
+
+        while(j >= 0) {
+            nums1[k] = nums2[j];
+            j--;
+            k--;
+        }
+    }
+
+
+    public static void main(String[] args) {
+
+    }
+
+
+
+}
